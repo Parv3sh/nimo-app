6660e01b45a4003acb8683b4e958d04a5d4c87dd
Parvesh Kumar <parveshk2@gmail.com>
2022-12-18 02:06:24 +1100

    added microservices
    Signed-off-by: Parvesh Kumar <parveshk2@gmail.com>

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -1,49 +1,64 @@
-// Create clients and set shared const values outside of the handler.
+import axios from "axios";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
-// Create a DocumentClient that represents the query to add an item
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-// Get the DynamoDB table name from environment variables
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.PRICE_TABLE;
 
-/**
- * A simple example includes a HTTP post method to add one item to a DynamoDB table.
- */
 export const putItemHandler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
-    }
-    // All log statements are written to CloudWatch
-    console.info('received:', event);
+  if (event.httpMethod !== "POST") {
+    throw new Error(
+      `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`
+    );
+  }
+  console.info("received:", event);
 
-    // Get id and name from the body of the request
-    const body = JSON.parse(event.body);
-    const id = body.id;
-    const name = body.name;
+  const id = event.pathParameters.id;
+  const url = `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=aud`;
+  const res = await axios.get(url);
+  const data = res.data;
 
-    // Creates a new item, or replaces an old item with a new item
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
-    var params = {
-        TableName : tableName,
-        Item: { id : id, name: name }
-    };
+  const sesClient = new SESClient({});
+  const sesParams = {
+    Destination: {
+      ToAddresses: ["parveshk2+nimo@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: `The price of ${id} is ${data[id].aud}` },
+        Html: {
+          Data: `<html><head><style>h1 {font-size: 100px;}</style></head><body><h1>The price of ${id} is ${data[id].aud} AUD</h1></body></html>`,
+        },
+      },
+      Subject: { Data: "Price Alert" },
+    },
+    Source: "parveshk2+nimo@gmail.com",
+  };
+  const sesData = await sesClient.send(new SendEmailCommand(sesParams));
+  console.log("Success - email sent", sesData);
 
-    try {
-        const data = await ddbDocClient.send(new PutCommand(params));
-        console.log("Success - item added or updated", data);
-      } catch (err) {
-        console.log("Error", err.stack);
-      }
+  const params = {
+    TableName: tableName,
+    Item: { id: new Date().toISOString(), name: id, price: data[id].aud },
+  };
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(body)
-    };
+  try {
+    const data = await ddbDocClient.send(new PutCommand(params));
+    console.log("Success - item added or updated", data);
+  } catch (err) {
+    console.log("Error", err.stack);
+  }
 
-    // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
 };
